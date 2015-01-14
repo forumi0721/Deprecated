5eb6d11bfe3d38f19bddc92fe0c53a99eb685f5a
forumi0721 <forumi0721@gmail.com>
2015-01-14 18:29:33 +0900

    Add repo-gen.sh

---
+++ repo-gen.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+if [ -z "$(which repo-add 2> /dev/null)" ]; then
+	echo "command not found : repo-add"
+	exit 1
+fi
+
+rm -rf repo
+archs=($(find . -name *.pkg.tar.* | sed -e 's/^.*-//g' -e 's/\..*$//g' | sort -u | grep -v any))
+for archdir in ${archs[@]}
+do
+	mkdir -p repo/${archdir}
+done
+for pkg in $(find . -name *.pkg.tar.*)
+do
+	echo "Generate ${pkg}"
+	arch=$(echo $pkg | sed -e 's/^.*-//g' -e 's/\..*$//g')
+	if [ "${arch}" = "any" ]; then
+		for archdir in ${archs[@]}
+		do
+			cp $pkg repo/${archdir}/
+			pushd . &> /dev/null
+			cd repo/${archdir}
+			repo-add StoneCold-BPi.db.tar.gz "$(basename "${pkg}")"
+			repo-add -f StoneCold-BPi.files.tar.gz "$(basename "${pkg}")"
+			popd &> /dev/null
+		done
+	else
+		cp $pkg repo/${arch}/
+		pushd . &> /dev/null
+		cd repo/${arch}
+		repo-add StoneCold-BPi.db.tar.gz "$(basename "${pkg}")"
+		repo-add -f StoneCold-BPi.files.tar.gz "$(basename "${pkg}")"
+		popd &> /dev/null
+	fi
+done
+
+if [ ! -e /media/StoneColdNAS/nas_htdocs ]; then
+	if [ ! -z "$(which mnt)" ]; then
+		mnt
+	fi
+fi
+
+if [ -e /media/StoneColdNAS/nas_htdocs ]; then
+	rm -rf /media/StoneColdNAS/nas_htdocs/arch/stonecold-bpi
+	mkdir -p /media/StoneColdNAS/nas_htdocs/arch
+	cp -r repo /media/StoneColdNAS/nas_htdocs/arch/stonecold-bpi
+fi
+
